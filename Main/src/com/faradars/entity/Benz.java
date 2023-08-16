2f8fa50ebb0521cd4fc20b11d2a9aa9351978914
HP <firoozinia1123@yahoo.com>
2023-08-16 09:19:27 +0330

    Banz Ali

--- Main/src/com/faradars/entity/Benz.java
+++ Main/src/com/faradars/entity/Benz.java
@@ -1,8 +1,8 @@
 package com.faradars.entity;
 
 public class Benz implements ICar {
-    public static final String NAME="Benz   ";
-    public static final int MAX_SPEED =290;
+    public static final String NAME="Beniiiz   ";
+    public static final int MAX_SPEED =150;
 
     public String getName(){
         return NAME;
